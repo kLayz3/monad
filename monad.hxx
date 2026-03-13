f946e008c71f4d8d4955a52873c156214a083fe1
𝒦𝓁𝒶𝓎𝓏𝓮 <M.Bajzek@gsi.de>
2026-03-13 12:56:26 +0100

    Added small predicate function (moved from M.B. phd project to here. Brevity)

--- monad.hxx
+++ monad.hxx
@@ -331,6 +331,17 @@ void Erase(std::vector<T>& v, Predicate p) noexcept (
 template<typename T>
 T rround(double x) noexcept { return static_cast<T>(x + 0.5); }
 
+template<typename T, typename U>
+bool IsInside(const T& value, const std::array<U,2>& bounds) {
+	static_assert (
+		std::is_convertible_v<decltype(std::declval<const U&>() <= std::declval<const T&>()), bool> &&
+		std::is_convertible_v<decltype(std::declval<const T&>() <  std::declval<const U&>()), bool>,
+		"T and U must support comparison operators T <= U and U < T."
+	);
+	return bounds[0] <= value and value < bounds[1];
+}
+
+
 using std::chrono::duration_cast;
 using std::chrono::seconds;
 using std::chrono::milliseconds;
