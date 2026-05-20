1e0dfe5fe2cd48d1be9db2c50b02a09d334ba2c7
𝒦𝓁𝒶𝓎𝓏𝓮 <M.Bajzek@gsi.de>
2026-05-20 13:48:49 +0200

    Few extra flags for BOOST in case of debug builds

--- common.mk
+++ common.mk
@@ -7,7 +7,10 @@ CXXFLAGS += -DPOOL_MAX_THREADS_=$(shell nproc) \
 
 ifneq ($(OPTIMIZATION), 1)
 
-CXXFLAGS += -ggdb3 -g3 -O0 -fno-omit-frame-pointer -fno-inline
+CXXFLAGS += -ggdb -g -O0 -fno-omit-frame-pointer -fno-inline \
+	-DBOOST_STACKTRACE_USE_ADDR2LINE \
+	-lboost_stacktrace_addr2line -ldl
+
 export ASAN_OPTIONS=detect_leaks=1,strict_string_checks=1,alloc_dealloc_mismatch=1
 export MALLOC_CHECK_=3
 export GLIBC_TUNABLES=glibc.malloc.tcache_count=0
